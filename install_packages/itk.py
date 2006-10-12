c8d12e1f1e890d742dc4dad62e783ad463b8e3bc
cpbotha <cpbotha@localhost>
2006-10-12 14:14:53 +0000

    Changed SVN update date.

--- install_packages/itk.py
+++ install_packages/itk.py
@@ -8,10 +8,10 @@ import sys
 BASENAME = "Insight"
 # password part of REPO spec
 CVS_REPO = ":pserver:anonymous:insight@www.itk.org:/cvsroot/" + BASENAME
-CVS_VERSION = "-D 20061010"
+CVS_VERSION = "-D 20061012" # wrapitk+itk fix by Gaetan
 
 CABLESWIG_REPO = ":pserver:anonymous@www.itk.org:/cvsroot/CableSwig"
-CABLESWIG_VERSION = "-D 20061010"
+CABLESWIG_VERSION = "-D 20061012" # wrapitk+itk fix by Gaetan
 
 class ITK(InstallPackage):
     
