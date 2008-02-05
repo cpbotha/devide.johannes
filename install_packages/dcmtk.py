b8ac8b2ba6eaebf8ecf9a9b9400a5cbb94917902
cpbotha <cpbotha@localhost>
2008-02-05 17:39:13 +0000

    DCMTK install package (ptoooey) now works on windows.

--- install_packages/dcmtk.py
+++ install_packages/dcmtk.py
@@ -15,11 +15,20 @@ DCMTK_URL = "ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/" \
 DCMTK_DIRBASE = "dcmtk-3.5.4"
 
 class DCMTK(InstallPackage):
-    
+    """This is not the best example of a johannes install package.  In
+    fact, it's probably the worst example, mostly because DCMTK builds
+    like crap.  autoconf on unix, cmake on windows, what's up with
+    that?
+
+    In anycase, it works.  Please just ignore it and let it do its
+    job.
+    """
+
     def __init__(self):
         self.tbfilename = os.path.join(config.archive_dir,
                 DCMTK_ARCHIVE)
         self.build_dir = os.path.join(config.build_dir, DCMTK_DIRBASE)
+        self.source_dir = self.build_dir
         self.inst_dir = os.path.join(config.inst_dir, 'dcmtk')
 
     def get(self):
@@ -66,17 +75,59 @@ class DCMTK(InstallPackage):
 
 
     def configure_nt(self):
+        if os.path.exists(
+            os.path.join(self.build_dir, 'CMakeFiles/cmake.check_cache')):
+            utils.output("DCMTK build already configured.")
+            return
+
         # modify CMakeLists, changing occurrences of /MT into /MD and
         # /MTd into /MDd
-        pass
+        os.chdir(self.build_dir)
+        repls = [('\/MT', '/MD')]
+        utils.re_sub_filter_file(repls, 'CMakeLists.txt') 
+
+        # then run cmake
+        cmake_params = "-DCMAKE_INSTALL_PREFIX=%s " % (self.inst_dir,)
+        ret = utils.cmake_command(self.build_dir, self.source_dir,
+                cmake_params)
+
+        if ret != 0:
+            utils.error("Could not configure DCMTK.  Fix and try again.")
 
     def configure(self):
         if os.name == 'nt':
-            configure_nt()
+            self.configure_nt()
         else:
-            configure_posix()
+            self.configure_posix()
 
-    def build(self):
+    def build_nt(self):
+        os.chdir(self.build_dir)
+        # do check for some file
+
+        if os.path.exists(os.path.join('dcmdata/libsrc',
+            config.BUILD_TARGET, 'dcmdata.lib')):
+            utils.output('dcmtk::dcmdata already built.  Skipping.')
+
+        else:
+            ret = utils.make_command('dcmtk.sln', install=False,
+                    project='dcmdata')
+
+            if ret != 0:
+                utils.error('Could not build dcmtk::dcmdata.')
+
+        if os.path.exists(os.path.join('ofstd/libsrc',
+            config.BUILD_TARGET, 'ofstd.lib')):
+            utils.output('dcmtk::ofstd already built.  Skipping.')
+
+        else:
+            ret = utils.make_command('dcmtk.sln', install=False,
+                    project='ofstd')
+
+            if ret != 0:
+                utils.error('Could not build dcmtk::ofstd.')
+
+
+    def build_posix(self):
         os.chdir(self.build_dir)
         if os.path.exists("dcmdata/libsrc/libdcmdata.so"):
             utils.output("DCMTK already built. Skipping.")
@@ -91,7 +142,40 @@ class DCMTK(InstallPackage):
             if ret != 0:
                 utils.error('Could not build dcmtk.  Fix and try again.')
 
-    def install(self):
+    def build(self):
+        if os.name == 'nt':
+            self.build_nt()
+        else:
+            self.build_posix()
+
+    def install_nt(self):
+        if os.path.exists(self.inst_dir):
+            utils.output("DCMTK already installed. Skipping.")
+
+        else:
+            # we have to be in the build_dir
+            os.chdir(self.build_dir)
+
+            lib_dir = os.path.join(self.inst_dir, 'lib')
+            inc_dir = os.path.join(self.inst_dir, 'include', 'dcmtk')
+            os.makedirs(lib_dir)
+            os.makedirs(inc_dir)
+
+            for modname in ['dcmdata', 'ofstd']:
+                lib_src_fn = os.path.join('%s/libsrc' % (modname,), 
+                    config.BUILD_TARGET, '%s.lib' % (modname,))
+                lib_dst_fn = os.path.join(lib_dir, 
+                        '%s.lib' % (modname,))
+
+                shutil.copyfile(lib_src_fn, lib_dst_fn)
+
+            for modname in ['dcmdata', 'ofstd', 'config']:
+                inc_src_dir = os.path.join(modname, 'include',
+                        'dcmtk', modname)
+                inc_dst_dir = os.path.join(inc_dir, modname)
+                shutil.copytree(inc_src_dir, inc_dst_dir)
+
+    def install_posix(self):
         if os.path.exists(self.inst_dir):
             utils.output("DCMTK already installed. Skipping.")
 
@@ -110,7 +194,13 @@ class DCMTK(InstallPackage):
                 utils.error(
                     'DCMTK make install-lib failed.  Fix and try again.')
 
-                
+
+
+    def install(self):
+        if os.name == 'nt':
+            self.install_nt()
+        else:
+            self.install_posix()
 
         # either way, we have to register our binary path with config
         config.DCMTK_INCLUDE = os.path.join(self.inst_dir, 'include')
