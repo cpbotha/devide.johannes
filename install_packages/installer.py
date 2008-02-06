8192ab576c304dba55fb25b445261176e1fdcdf1
cpbotha <cpbotha@localhost>
2008-02-06 17:03:12 +0000

    Ported installer install package to Windows.

--- install_packages/installer.py
+++ install_packages/installer.py
@@ -5,22 +5,35 @@ import shutil
 import sys
 import utils
 
-INSTALLER_TARBALL = "pyinstaller_1.2.tar.gz"
-INSTALLER_URL = "http://pyinstaller.hpcf.upr.edu/source/1.2/%s" % \
-                (INSTALLER_TARBALL,)
-INSTALLER_DIRBASE = "pyinstaller_1.2"
+# change this to upgrade the pyinstaller version
+INSTALLER_VER = "1.3"
+
+INSTALLER_BASENAME = "pyinstaller"
+# directory has dash
+INSTALLER_DIRBASE = INSTALLER_BASENAME + "-" + INSTALLER_VER
+# archive has underscore
+INSTALLER_ARCBASE = INSTALLER_BASENAME + "_" + INSTALLER_VER
+
+if os.name == 'nt':
+    INSTALLER_ARCHIVE = INSTALLER_ARCBASE + ".zip"
+else:
+    INSTALLER_ARCHIVE = INSTALLER_ARCBASE + ".tar.gz"
+
+INSTALLER_URL = "http://pyinstaller.hpcf.upr.edu/source/%s/%s" % \
+                (INSTALLER_VER, INSTALLER_ARCHIVE)
 
 class Installer(InstallPackage):
 
     def __init__(self):
-        self.tbfilename = os.path.join(config.archive_dir, INSTALLER_TARBALL)
+        self.tbfilename = os.path.join(config.archive_dir,
+                INSTALLER_ARCHIVE)
         self.build_dir = os.path.join(config.build_dir, INSTALLER_DIRBASE)
         self.inst_dir = os.path.join(config.inst_dir, 'installer')
 
     def get(self):
         if os.path.exists(self.tbfilename):
             utils.output("%s already present, not downloading." %
-                         (INSTALLER_TARBALL,))
+                         (INSTALLER_ARCHIVE,))
         else:
             utils.goto_archive()
             utils.urlget(INSTALLER_URL)
@@ -32,12 +45,30 @@ class Installer(InstallPackage):
             utils.output("Unpacking INSTALLER source.")
             utils.unpack_build(self.tbfilename)
             # and we need to delete the irritating optparse.py from
-            # the installer directory
+            # the installer directory (it's only needed for older
+            # pythons, and tends to break things in johannes)
             os.unlink(os.path.join(self.build_dir, 'optparse.py'))
 
     def configure(self):
         pass
-    
+
+    def build_posix_part(self):
+        os.chdir(os.path.join('source', 'linux'))
+
+        ret = os.system('%s Make.py' % (sys.executable,))
+        if ret != 0:
+            utils.error('Error creating make file.  Fix and try again.')
+
+        # on GCCs with ProPolice, the stupid thing thinks that the
+        # McMillan installer is trying to smash the stack.
+        utils.re_sub_filter_file([("^CFLAGS=(.*)$","CFLAGS=\\1 "
+                                   "-fno-stack-protector")],
+                                 'Makefile')
+
+        ret = os.system('make')
+        if ret != 0:
+            utils.error('Could not build stub.  Fix and try again.')
+
     def build(self):
         os.chdir(self.build_dir)
 
@@ -47,27 +78,13 @@ class Installer(InstallPackage):
             utils.output('installer already built.  Skipping step.')
 
         else:
-            os.chdir(os.path.join('source', 'linux'))
-
-            ret = os.system('%s Make.py' % (sys.executable,))
-            if ret != 0:
-                utils.error('Error creating make file.  Fix and try again.')
-
-            # on GCCs with ProPolice, the stupid thing thinks that the
-            # McMillan installer is trying to smash the stack.
-            utils.re_sub_filter_file([("^CFLAGS=(.*)$","CFLAGS=\\1 "
-                                       "-fno-stack-protector")],
-                                     'Makefile')
-
-            ret = os.system('make')
-            if ret != 0:
-                utils.error('Could not build stub.  Fix and try again.')
+            if os.name == 'posix':
+                self.build_posix_part()
 
             os.chdir(self.build_dir)
             ret = os.system('%s Configure.py' % (sys.executable,))
             if ret != 0:
                 utils.error('Error pre-configuring.  Fix and try again.')
-            
 
     def install(self):
         # set variables here
