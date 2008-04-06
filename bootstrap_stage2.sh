808aff4f39e5c0642f61e7a6532b5c3dd96a811f
cpbotha <cpbotha@localhost>
2008-04-06 17:46:18 +0000

    Updated johannes to Python to 2.5.2.  Not tested yet.

--- bootstrap_stage2.sh
+++ bootstrap_stage2.sh
@@ -82,8 +82,8 @@ fi
 
 
 cd build
-tar xjvf ../archive/Python-2.5.1.tar.bz2
-cd Python-2.5.1
+tar xjvf ../archive/Python-2.5.2.tar.bz2
+cd Python-2.5.2
 ./configure --enable-shared --prefix=$WD/inst/python
 
 if [ "$?" -ne "0" ]; then
