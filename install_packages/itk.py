4ff900d699e983c13d1c6ccc1a373cd82d9e6573
cpbotha <cpbotha@localhost>
2008-07-31 20:29:00 +0000

    Updated johannes to ITK 3.8.

--- install_packages/itk.py
+++ install_packages/itk.py
@@ -12,7 +12,7 @@ import sys
 BASENAME = "Insight"
 # password part of REPO spec
 CVS_REPO = ":pserver:anonymous:insight@www.itk.org:/cvsroot/" + BASENAME
-CVS_VERSION = "-r ITK-3-6" # 
+CVS_VERSION = "-r ITK-3-8" # 
 
 CS_BASENAME = "CableSwig"
 # password part of REPO spec
