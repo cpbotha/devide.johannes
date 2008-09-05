b0aad45a2cc2ab1a0b95d3b71bb9cf0fc2faf7ca
cpbotha <cpbotha@localhost>
2008-09-05 20:56:22 +0000

    Small updates to begin building special ASCI A17 version.

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ import sys
 # build it, so it is important that you change this timestamp so that
 # config.py gets updated with a new revision keyword when you are
 # shipping a new devide.
-STAMP = "20080629-2200"
+STAMP = "20080905-2300"
 JOHANNES_REV = "$Revision$"
 JOHANNES_REL = JOHANNES_REV.split()[1]
 
