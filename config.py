3b1cf98a221cbd0a61c805fc42ec83354bca44d8
cpbotha <cpbotha@localhost>
2008-09-06 22:41:17 +0000

    Updated stamp to get CodeRunner fix in DeVIDE build.

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ import sys
 # build it, so it is important that you change this timestamp so that
 # config.py gets updated with a new revision keyword when you are
 # shipping a new devide.
-STAMP = "20080906-0100"
+STAMP = "20080907-0046"
 JOHANNES_REV = "$Revision$"
 JOHANNES_REL = JOHANNES_REV.split()[1]
 
