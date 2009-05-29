2edad7c009a38ac3d38fcb50a527fbe8c00307de
cpbotha <cpbotha@localhost>
2009-05-29 15:07:59 +0000

    Now able to replace bootstrap_stage1.sh.

--- bootstrap.py
+++ bootstrap.py
@@ -9,8 +9,31 @@
 # 3. johannes.py will be run by the python that is locally built by
 # EITHER bootstrap.py OR bootstrap_stage{1,2}.sh
 
+import config
 import getopt
+import os
 import sys
+import utils
+
+def download_python():
+    urlbase = 'http://python.org/ftp/python/2.5.4'
+    if os.name == 'posix':
+        fname = 'Python-2.5.4.tar.bz2'
+        url = '%s/%s' % (urlbase, fname)
+    elif os.name == 'nt':
+        import platform
+        a = platform.architecture()[0]
+        if a == '32bit':
+            fname = 'python-2.5.4.msi'
+            url = '%s/%s' % (urlbase, fname) 
+        else:
+            fname = 'python-2.5.4.amd64.msi'
+            url = '%s/%s' % (urlbase, fname)
+
+    utils.goto_archive()
+    utils.urlget(url)
+
+    return fname
 
 def usage():
     message = """
@@ -18,10 +41,12 @@ Invoke with:
     python bootstrap.py -w working_directory
     """
 
+    print message
+
 def main():
     try:
         optlist, args = getopt.getopt(
-                sys.argv[1:], 'w',
+                sys.argv[1:], 'w:',
                 ['working-dir='])
 
     except getopt.GetoptError,e:
@@ -30,6 +55,7 @@ def main():
 
     working_dir = None
 
+    print optlist
     for o, a in optlist:
         if o in ('-w', '--working-dir'):
             working_dir = a
@@ -37,6 +63,34 @@ def main():
     if not working_dir:
         usage()
         return
+
+    # this will setup the necessary dirs for later calls into utils
+    config.init(working_dir, None)
+
+    # first create directory structure
+    prepare_dirs(working_dir)
+
+    # now download the python (source for linux, binaries for windows)
+    python_fname = download_python()
+
+    if os.name == 'nt':
+        # this means we just have to unpack python
+        pass
+
+    else:
+        # this means we have to test for dependencies and then build
+        # Python.
+
+def prepare_dirs(working_dir):
+    a_dir = os.path.join(working_dir, 'archive')
+    b_dir = os.path.join(working_dir, 'build')
+    i_dir = os.path.join(working_dir, 'inst')
+
+    for d in [working_dir, a_dir, b_dir, i_dir]:
+        if not os.path.exists(d):
+            os.mkdir(d)
+
+
            
 
 if __name__ == '__main__':
