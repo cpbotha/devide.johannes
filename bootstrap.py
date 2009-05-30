099ed9b162e6c77a1360dc2d508b5810be3d2004
cpbotha <cpbotha@localhost>
2009-05-30 11:56:53 +0000

    Now able to download, do local install and setup invocation script for local python on Windows.

--- bootstrap.py
+++ bootstrap.py
@@ -15,6 +15,15 @@ import os
 import sys
 import utils
 
+nt_python = """
+@echo off
+@rem script to run locally installed johannes python
+@rem should be located in johannes wd\jpython.sh
+@rem as it assumes the local install of python is in
+@rem wd\inst\python and it's in wd
+%~dp0\inst\python\python.exe %1 %2 %3 %4 %5 %6 %7 %8 %9 
+"""
+
 def download_python():
     urlbase = 'http://python.org/ftp/python/2.5.4'
     if os.name == 'posix':
@@ -75,11 +84,43 @@ def main():
 
     if os.name == 'nt':
         # this means we just have to unpack python
-        pass
+        py_msi_dir = os.path.join(config.archive_dir, python_fname)
+        py_inst_dir = os.path.join(config.inst_dir, 'python')
+
+        if os.path.exists(py_inst_dir):
+            utils.output(
+            'Python installation dir present.  Skipping install.')
+
+        else:
+            utils.output('Doing local installation of Python.')
+
+            # run with basic interface
+            # ret is 0 if successful
+            ret = os.system(
+                    'msiexec /a %s TARGETDIR=%s /qb' % 
+                    (py_msi_dir, py_inst_dir))
+
+            if ret != 0:
+                utils.error(
+                        'Failed locally installing Python.  EFS / msiexec problems?')
+
+        jpcmd = 'jpython.cmd'
+        f = open(os.path.join(config.working_dir, jpcmd), 'w')
+        f.write(nt_python)
+        f.close()
 
     else:
         # this means we have to test for dependencies and then build
         # Python.
+        jpcmd = 'jpython.sh'
+
+    print """
+Successfully bootstrapped local johannes Python.  Start the full build
+system with:
+%s johannes.py -w %s
+    """ % \
+    (os.path.join(config.working_dir, jpcmd), config.working_dir)
+
 
 def prepare_dirs(working_dir):
     a_dir = os.path.join(working_dir, 'archive')
