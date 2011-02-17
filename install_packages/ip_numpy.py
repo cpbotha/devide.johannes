1745a12d47e502b295593fee960698d48d32ee10
cpbotha <cpbotha@localhost>
2011-02-17 10:15:06 +0000

    using Gohlke numpy packages (win32,win64)

--- install_packages/ip_numpy.py
+++ install_packages/ip_numpy.py
@@ -13,7 +13,6 @@ NUMPY_VER = "1.5.1"
 NUMPY_BASENAME = "numpy-" + NUMPY_VER
 NUMPY_DIRBASE = NUMPY_BASENAME 
 
-WIN64 = False
 if os.name == 'posix':
     NUMPY_ARCHIVE = "%s.tar.gz" % (NUMPY_BASENAME,)
     NUMPY_URL = "http://sourceforge.net/projects/numpy/files/NumPy/%s/%s/download" % (NUMPY_VER, NUMPY_ARCHIVE)
@@ -22,10 +21,10 @@ elif os.name == 'nt':
     NUMPY_URL_BASE = "http://graphics.tudelft.nl/~cpbotha/files/devide/johannes_support/gohlke/%s"
 
     if config.WINARCH == '32bit':
-        NUMPY_ARCHIVE = "%s-win32-py2.7.exe" % (NUMPY_BASENAME,)   
+        NUMPY_ARCHIVE = "%s.win32-py2.7.exe" % (NUMPY_BASENAME,)   
 
     else:
-        NUMPY_ARCHIVE = "%s-win-amd64-py2.7.zip" % (NUMPY_BASENAME,)   
+        NUMPY_ARCHIVE = "%s.win-amd64-py2.7.exe" % (NUMPY_BASENAME,)   
 
     # now construct the full URL
     NUMPY_URL = NUMPY_URL_BASE % (NUMPY_ARCHIVE,)
@@ -48,18 +47,17 @@ class NumPy(InstallPackage):
             utils.urlget(NUMPY_URL, NUMPY_ARCHIVE)
 
     def unpack(self):
-        if os.name == 'posix':
-            if os.path.isdir(self.build_dir):
-                utils.output("NUMPY source already unpacked, not redoing.")
-            else:
-                utils.output("Unpacking NUMPY source.")
-                utils.unpack_build(self.tbfilename)
+        if os.path.isdir(self.build_dir):
+            utils.output("NUMPY source already unpacked, not redoing.")
+            return
 
+        utils.output("Unpacking NUMPY source.")
+        if os.name == 'posix':
+            utils.unpack_build(self.tbfilename)
         else:
-            utils.output("Nothing to unpack (Windows).")
-	    # make the build dir
-	    # unpack in there
-	    # copy contents of platlibs to python/Lib/site-packages
+            os.mkdir(self.build_dir)
+            os.chdir(self.build_dir)
+            utils.unpack(self.tbfilename)
 
     def configure(self):
         pass
@@ -67,30 +65,27 @@ class NumPy(InstallPackage):
     def build(self):
         if os.name == 'nt':
             utils.output("Nothing to build (Windows).")
-            return
-
-        # for posix, we have to build the whole shebang.
-        os.chdir(self.build_dir)
-
-        # weak test... there are .so files deeper, but they're in platform
-        # specific directories
-        if os.path.exists('build'):
-            utils.output('numpy already built.  Skipping step.')
 
         else:
-            # the build_ext -lg2c is needed on the VLE Centos3 system, else
-            # we get lapack related (symbol not found) errors at import numpy
-            #ret = os.system('%s setup.py build build_ext -lg2c' % (sys.executable,))
-            ret = os.system('%s setup.py build build_ext' % (sys.executable,))
-            
-            if ret != 0:
-                utils.error('numpy build failed.  Please fix and try again.')
+
+            # for posix, we have to build the whole shebang.
+            os.chdir(self.build_dir)
+
+            # weak test... there are .so files deeper, but they're in platform
+            # specific directories
+            if os.path.exists('build'):
+                utils.output('numpy already built.  Skipping step.')
+
+            else:
+                # the build_ext -lg2c is needed on the VLE Centos3 system, else
+                # we get lapack related (symbol not found) errors at import numpy
+                #ret = os.system('%s setup.py build build_ext -lg2c' % (sys.executable,))
+                ret = os.system('%s setup.py build build_ext' % (sys.executable,))
+                
+                if ret != 0:
+                    utils.error('numpy build failed.  Please fix and try again.')
 
     def install(self):
-        if WIN64:
-            utils.output("numpy not yet supported on Win64.")
-            return
-
         # to test for install, just do python -c "import numpy"
         # and test the result (we could just import directly, but that would
         # only work once our invoking python has been stopped and started
@@ -116,8 +111,11 @@ class NumPy(InstallPackage):
                 # unpack relevant ZIP into Python site-packages dir.
                 from distutils import sysconfig
                 spd = sysconfig.get_python_lib()
-                os.chdir(spd)
-                utils.unpack(self.tbfilename)
+
+                # copy self.build_dir/PLATLIB/numpy to python/libs/site-packages/numpy
+                # we're not copying *egg.info or SCRIPTS/f2py.py
+                pl_dir = os.path.join(self.build_dir, 'PLATLIB')
+                shutil.copytree(os.path.join(pl_dir,'numpy'), os.path.join(spd, 'numpy'), symlinks=True)
 
     def clean_build(self):
         utils.output("Removing build and install directories.")
@@ -131,9 +129,6 @@ class NumPy(InstallPackage):
             shutil.rmtree(numpy_instdir)
 
     def get_installed_version(self):
-        if WIN64:
-            return "No numpy on Win64."
-
         import numpy
         return numpy.__version__
 
