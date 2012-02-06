3b155a2331c6eda24b2e3425b8251368ae283a16
Charl Botha <c.p.botha@tudelft.nl>
2012-02-06 18:43:31 +0100

    Increased version to get more fixes.

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ import sys
 # which changeset of DeVIDE this johannes changeset is able to build
 # FIXME: change devide installpackage to find hg id of the johannes that
 # builds it.
-DEVIDE_CHANGESET_ID = "ebe71fdb1f18"
+DEVIDE_CHANGESET_ID = "29f40998b95f"
 # this should be the date of the above changeset ID
 # and probably the new-style DeVIDE versioning
 # so for release: DeVIDE v11.9.16
